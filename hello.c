fd25e9ee14dfe11ab7c013fbdeb90449d96b5ef5
Devanshu <devanshusomani99@gmail.com>
2018-03-01 16:50:35 +0530

    step 25

--- hello.c
+++ hello.c
@@ -5,11 +5,6 @@ void microkernel_sendmsg(char *);
 int main (){
     printf("Hello World!\n");
     printf("This must be a monolithic design\n");
-    microkernel_sendmsg("is more portable");
-}
-
-void microkernel_sendmsg(char *a){
-    printf("microkernel: %s\n", a);
 }
 
 void microkernel_getmsg(char *b){
